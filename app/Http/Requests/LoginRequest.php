5a25019c9c043ebbb5183cb6dad72a471dc20571
Mohammad Arkan <mohammadarkan0101@gmail.com>
2026-08-14 00:21:23 +0700

    update-laravel-project

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -29,7 +29,7 @@ class LoginRequest extends FormRequest
         return [
             'email'    => $this->email,
             'password' => $this->password,
-            'status'   => true,
+            'status'   => TRUE,
         ];
     }
 
