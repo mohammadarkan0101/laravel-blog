00406ae1766f59c23481e5fefd12f6b590bc7e52
Mohammad Arkan <mohammadarkan0101@gmail.com>
2026-08-26 10:38:38 +0700

    update-laravel-project

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Spatie\Permission\Traits\HasRoles;
 
